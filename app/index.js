7aad5b00cd92e33b0ffaf8a9cc622f09381c34c5
2233540467@qq.com <2233540467@qq.com>
2016-09-21 09:06:19 +0800

    add jquery component

--- app/index.js
+++ app/index.js
@@ -1,19 +1,27 @@
 /**
  * Created by Gaven on 2016/9/12.
  */
-import React from 'react';
+import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import Login from './components/Login/Login'
-import { store } from './store/store';
+import { store } from './store/store'
+import $ from 'jquery'
 // import {setstate} from './actions/actions';
 // console.log(store.getState());
 // store.dispatch(setstate('hehe'));
 // console.log(store.getState());
 
+const get163 = (store)=>{
+    $.get('http://3g.163.com/touch/reconstruct/article/list/BA8D4A3Rwangning/0-10.html', function (data){
+        console.log(data)
+        console.log(store)
+    });
+}
+
 render(
-    <Provider store={store}>
-        <Login setInfo={(e) => { console.log(e) }} />
+    <Provider store={ store }>
+        <Login setInfo={ get163 } />
     </Provider>,
     document.querySelector("#app")
 );
